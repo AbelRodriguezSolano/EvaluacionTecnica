59805278f7805c7e96198590c11d0b3d4953c3a5
Abel Rodriguez <abrodriguez@semma.gob.do>
2025-11-28 18:59:01 -0400

    Add password hashing service

---
+++ src/EvaluacionTecnica.Application/Services/PasswordHasher.cs
@@ -0,0 +1,16 @@
+using EvaluacionTecnica.Application.Interfaces;
+
+namespace EvaluacionTecnica.Application.Services;
+
+public class PasswordHasher : IPasswordHasher
+{
+    public string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool VerifyPassword(string password, string hashedPassword)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+    }
+}
