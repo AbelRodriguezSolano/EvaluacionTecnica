a0e63281a8aecde1a45d03358517387cfdb99688
Abel Rodriguez <abrodriguez@semma.gob.do>
2025-11-28 17:39:31 -0400

    Add initial migration with seed data

---
+++ src/EvaluacionTecnica.Infrastructure/Migrations/20251128211247_InitialCreate.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EvaluacionTecnica.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Usuario_Transaccion = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Fecha_Transaccion = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleId = table.Column<int>(type: "int", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Cedula = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Usuario_Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Contraseña = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Fecha_Nacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Usuario_Transaccion = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Fecha_Transaccion = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Fecha_Transaccion", "Nombre", "Usuario_Transaccion" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "ADMIN", "SYSTEM" },
+                    { 2, new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "DESARROLLADOR", "SYSTEM" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Apellido", "Cedula", "Contraseña", "Fecha_Nacimiento", "Fecha_Transaccion", "Nombre", "RoleId", "Usuario_Nombre", "Usuario_Transaccion" },
+                values: new object[,]
+                {
+                    { 1, "Consulting", "25322522135", "ADMIN", new DateTime(1990, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Simetrica", 1, "ADMIN", "SYSTEM" },
+                    { 2, "Consulting", "0000000000", "APLICANTE", new DateTime(2000, 2, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Nombre_desarrollador", 2, "DESARROLLADOR", "SYSTEM" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_Nombre",
+                table: "Roles",
+                column: "Nombre",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Cedula",
+                table: "Users",
+                column: "Cedula",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Usuario_Nombre",
+                table: "Users",
+                column: "Usuario_Nombre",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
